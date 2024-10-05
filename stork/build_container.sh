c1fb40bcbc039ac3e2e1ed03c3ef544c5555ed09
Aaron U'Ren <aauren@gmail.com>
2024-10-05 15:40:43 -0500

    stork: add ISC stork container build

---
+++ stork/build_container.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+CURRENT_VERSION="1.0.0"
+
+docker build -t aauren/stork:${CURRENT_VERSION} -t aauren/stork:latest .
+
+docker push aauren/stork:${CURRENT_VERSION}
+docker push aauren/stork:latest
