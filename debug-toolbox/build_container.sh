0dbcb43ba04ad15716ac87daf1961a68186d99b8
Aaron U'Ren <aauren@gmail.com>
2025-01-10 16:17:51 -0600

    build_container.sh: bump debug-toolbox 0.0.3->0.0.4

--- debug-toolbox/build_container.sh
+++ debug-toolbox/build_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CURRENT_VERSION="0.0.3"
+CURRENT_VERSION="0.0.4"
 
-docker build -t aauren/debug-toolbox:${CURRENT_VERSION} -t aauren/debug-toolbox:latest .
+docker build -t aauren/debug-toolbox:${CURRENT_VERSION} -t aauren/debug-toolbox:latest . || exit 1
 
-docker push aauren/debug-toolbox:${CURRENT_VERSION}
-docker push aauren/debug-toolbox:latest
+docker push aauren/debug-toolbox:${CURRENT_VERSION} || exit 1
+docker push aauren/debug-toolbox:latest || exit 1
