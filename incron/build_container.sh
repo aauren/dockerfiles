5adeb0e8a29826bd0dca3e74222e0ecf50babb9d
Aaron U'Ren <aauren@gmail.com>
2026-02-25 22:52:27 -0600

    feat(incron): bump version 1.1.1 -> 1.1.2

--- incron/build_container.sh
+++ incron/build_container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CURRENT_VERSION="1.1.1"
+CURRENT_VERSION="1.1.2"
 
-docker build -t aauren/incron:${CURRENT_VERSION} -t aauren/incron:latest . || exit 1
+docker build --pull --no-cache -t aauren/incron:${CURRENT_VERSION} -t aauren/incron:latest . || exit 1
 
 docker push aauren/incron:${CURRENT_VERSION} || exit 1
 docker push aauren/incron:latest || exit 1
