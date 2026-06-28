d35e29f29e827d0fd32520753076ef67b3e55209
Aaron U'Ren <aauren@gmail.com>
2026-06-28 11:17:17 -0500

    feat(debug-toolbox): bump version 1.0.0 -> 1.0.1

--- debug-toolbox/build_container.sh
+++ debug-toolbox/build_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_VERSION="1.0.0"
+CURRENT_VERSION="1.0.1"
 
 docker build --pull --no-cache -t aauren/debug-toolbox:${CURRENT_VERSION} -t aauren/debug-toolbox:latest . || exit 1
 
