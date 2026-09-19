37e6133e94429c870573a68ff301e205ce97d57a
Aaron U'Ren <aauren@gmail.com>
2026-09-19 18:03:41 -0500

    debug-toolbox: begin tracking by date 1.1.0 -> 20260919

--- debug-toolbox/build_container.sh
+++ debug-toolbox/build_container.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CURRENT_VERSION="1.1.0"
+CURRENT_VERSION="20260919"
 
 docker build --pull --no-cache -t aauren/debug-toolbox:${CURRENT_VERSION} -t aauren/debug-toolbox:latest . || exit 1
 
